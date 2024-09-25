d330448d4b6655a64376fcb77fa562c3d25d80b6
Jair0305 <chamaco_03@hotmail.es>
2024-09-25 11:16:17 -0600

    spome changes

---
+++ app/api/delete/route.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from 'next/server';
+import clientPromise from '@/app/lib/mongodb';
+import { ObjectId } from 'mongodb';
+
+export async function DELETE(req: NextRequest) {
+    try {
+        const client = await clientPromise;
+        const db = client.db('BuenProvecha');
+        const collection = db.collection('Food');
+
+        const { id } = await req.json();
+
+        const result = await collection.deleteOne({ _id: new ObjectId(id) });
+
+        if (result.deletedCount === 1) {
+            return NextResponse.json({ message: 'Food deleted successfully' }, { status: 200 });
+        } else {
+            return NextResponse.json({ error: 'Food not found' }, { status: 404 });
+        }
+    } catch (error: any) {
+        console.error('Error deleting document from MongoDB:', error.message);
+        return NextResponse.json({ error: `Error deleting document from MongoDB: ${error.message}` }, { status: 500 });
+    }
+}
